994ea32b98f19b6152cd7a896db28ac29349d78a
b-ausoj <49319850+b-ausoj@users.noreply.github.com>
2026-01-04 13:33:11 +0100

    added preloading for js files

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -27,18 +27,18 @@ const RESOURCES = {"assets/AssetManifest.bin": "bb2c584ff82dfeffc139f176e8391167
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "ed66c06a7f7c9ba23a2a0f4f3c987e70",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "9653a6ff86bcb0bcdfc43d025443b9de",
+"flutter_bootstrap.js": "388426c0f320dc07c74f1eccd27b5a75",
 "icons/Icon-192.png": "ed66c06a7f7c9ba23a2a0f4f3c987e70",
 "icons/Icon-512.png": "ed66c06a7f7c9ba23a2a0f4f3c987e70",
 "icons/Icon-maskable-192.png": "ed66c06a7f7c9ba23a2a0f4f3c987e70",
 "icons/Icon-maskable-512.png": "ed66c06a7f7c9ba23a2a0f4f3c987e70",
-"index.html": "6686908c6350f54c0451f7263f422e75",
-"/": "6686908c6350f54c0451f7263f422e75",
+"index.html": "0170a8a4b5f7a07781c94f925fed7ef3",
+"/": "0170a8a4b5f7a07781c94f925fed7ef3",
 "main.dart.js": "d62a24860c55f3dc016ac2ed0476670b",
 "manifest.json": "c4a781ee881b4499b84f0f8c10b462e6",
 "robots.txt": "42b6d3d3b3a50100106324f83b91aa59",
 "sitemap.xml": "07c0067fd7c5bc4548cdadf896fd4a0d",
-"version.json": "819c44fbba82ef93d78d2dd4b24b9030"};
+"version.json": "2511b05f1aecba96097690df94b9f374"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
